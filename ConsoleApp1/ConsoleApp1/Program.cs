e16c776b358a372ba6a350993173f0e12c5f6711
priscilla gomes <priscillagomes021@outlook.com>
2019-09-10 17:24:37 -0300

    primeira aula

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,14 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Teste");
-            Console.ReadKey();
+            // texto informativo para consultar o livro
+            // pelo numero de registro do livro
+           Console.WriteLine("Informe o livro a ser consultado:");
+            // parte do codigo que recebe as informações 
+            // de registro do livro e coloca na varavel 
+            // numeroDoLivro para utilizar
+            var numeroDoLivro = Console.ReadLine();
+            // Aqui realizo a comparação das informações 
+            // do livro informado com o que tenho disponivel 
+            // em estoque 
+            if (numeroDoLivro == "123456")
+            {
+                // informo que este livro já está alocado
+                Console.WriteLine("Livro Indisponivel!!!");
+                 Console.ReadKey();
+                //Finaliza o metodo 
+                return;
+            }
+            else 
+            {
+                Console.WriteLine("Deseja alocar o Livro? (1)sim (2)não");
+                var resposta = Console.ReadLine();
+                if(resposta == "1");
+                { 
+                    Console.WriteLine("Livro alocado");
+                    Console.ReadKey();
+                }
+
+            }
+
+
+            
+
         }
 
     }
