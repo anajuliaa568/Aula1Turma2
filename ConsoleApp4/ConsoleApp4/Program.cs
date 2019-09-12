aa067680bcfdaf96b838c14173b6e21f5b962ffd
priscilla gomes <priscillagomes021@outlook.com>
2019-09-12 17:25:30 -0300

    commit022

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoOFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = "Meu nome é Felipe Junior e minha mãe escolheu colocar junior em meu nome porque achava legal.";
+            var countFind = 0;
+
+            Console.WriteLine("Encontre a palavra de até 3 caracteres:");
+            var palavra = Console.ReadLine();
+
+            for (int i = 1; i < 11; i++)
+                // diminuir um quando for avançando.
+            {
+                var stringBuilder = $"{nome[i - 1]} - {nome[i]}-{nome[i + 1]}";
+
+                Console.WriteLine(stringBuilder);
+
+                if (stringBuilder.Contains("u-n")) countFind++;
+               
+            }
+            Console.WriteLine($"Quantidade total de 'um' {countFind}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LacosDeRepeticao/UtilizandoWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            while (count < 1000000)
+            {
+                //interno
+                // se tiver uma alta interação não é necessario a operação abaixo, apenas count++; que vai muito mais rapido.
+                Console.WriteLine($"Número: {count}");
+                count++;
+
+            }
+
+            Console.WriteLine("Terminou  loop");
+            Console.ReadKey();
+        }    
+
+    }
+}
