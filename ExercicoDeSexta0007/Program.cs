5888d64597f3f8005ff0472aa6fb67a106fd0018
Mavinier <mavinier26@hotmail.com>
2019-09-14 15:37:28 -0300

    ExercicioDeSexta

---
+++ ExercicoDeSexta0007/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicoDeSexta0007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o texto: ");
+            var texto = Console.ReadLine();
+
+            string teste = texto;
+            var count = teste.Split('a','e','i','o','u');
+
+            Console.WriteLine(Convert.ToString(count.Length - 1));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ exercicioDeSexta04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioDeSexta04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a palavra:");
+            var texto = Console.ReadLine();
+            string correctString = texto.Replace("banana", "laranja");
+           
+            Console.WriteLine(correctString);
+            Console.ReadKey();
+        }
+    }
+}
