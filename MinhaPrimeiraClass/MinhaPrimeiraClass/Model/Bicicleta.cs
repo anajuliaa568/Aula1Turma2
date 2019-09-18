f7190aadafb8d4806d69b58b0a82e88f8d785461
priscilla gomes <priscillagomes021@outlook.com>
2019-09-18 17:29:23 -0300

    AulaDeQuarta22

---
+++ MinhaPrimeiraClass/MinhaPrimeiraClass/Model/Bicicleta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraClass.Model
+{
+    class Bicicleta
+    {
+        public string Marca { get; set; }
+        //get: obte informação set:permite motificar o valor ou seja dá a informação para ele 
+
+        public int Pneus { get; } = 2;
+       
+    }
+}
---
+++ MinhaPrimeiraClass/MinhaPrimeiraClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using MinhaPrimeiraClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criamos o nossao proprio tipo 
+            var listaDeBicicleta = new List<Bicicleta>
+            {
+                new Bicicleta()
+                {
+                    Marca = "Monark"
+
+                },
+                new Bicicleta()
+                {
+                    Marca = "Caloi"
+                }
+            };
+            listaDeBicicleta.ForEach(i => Console.WriteLine($"BikeOn > marca:{i.Marca} Pneus:{i.Pneus}"));
+            Console.ReadKey();
+        }
+
+     
+    }
+                   
+}
+
