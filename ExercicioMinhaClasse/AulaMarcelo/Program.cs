991494bb9188788dbfdbc3b3ce149f4132a4f9a8
priscilla gomes <priscillagomes021@outlook.com>
2019-09-19 17:26:27 -0300

    Atividades de quinta19

---
+++ ExercicioMinhaClasse/AulaMarcelo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaMarcelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+            Random rdm = new Random();
+
+           // HashSet<int> dados = new HashSet<int>();
+           // dados.Add(10);
+           // dados.Add(20);
+           // dados.Add(30);
+
+            for (int i = 0; i < 10; i++)
+            {
+                //Gera um numero aleatório de 0 a 100
+                int ValorGeradoAleatoriamente = rdm.Next(100);
+                //verifica se a lista não contém este numero gerado
+                if (!numeros.Contains(ValorGeradoAleatoriamente))
+                {
+                    //Adiciona o número gerado pelo random (que não é repetido)
+
+                    numeros.Add(ValorGeradoAleatoriamente);
+                }
+                else
+                {
+                    i --;
+                }
+
+             //bool ehRepetido =  numeros.Contains(ValorGeradoAleatoriamente)
+                //    if (!ehRepetido)
+                    
+                   
+               // numeros.Add(ValorGeradoAleatoriamente);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExercicioMinhaClasse/ExercicioMinhaClasse02/Model/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMinhaClasse02.Model
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char Sexo { get; set; }
+        public double Altura { get; set; }
+
+
+    }
+}
