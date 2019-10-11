c90e1b0bcc50acb45bae63d696590a98820d35c4
900065 <900065@GREEN12>
2019-10-11 17:31:22 -0300

    11/10/2019

---
+++ ListagemDeBicilcetas/Bicicleta/Program.cs
@@ -0,0 +1,31 @@
+﻿using SistemaDeBikes.Controller;
+using SistemaDeBikes.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bicicletaa
+{
+    public class Program
+    {
+        static BicicletaController bibliotecaContext = new BicicletaController();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("o que tu quer fazer truta??????");
+            Console.WriteLine("1 - Cadastrar uma bike");
+            int opcaoEscolhida = int.Parse(Console.ReadLine());
+            switch (opcaoEscolhida)
+            {
+                case 1:
+                    bibliotecaContext.AdicionarBike(new Bicicleta() { Marca = "TESTE" , Modelo = "TESTE" , Valor = 205});
+                        break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ ListagemDeBicilcetas/SistemaDeBikes/Controller/BicicletaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaDeBikes.Model;
+using SistemaDeBikes.Controller;
+
+namespace SistemaDeBikes.Controller
+{
+   
+    public class BicicletaController
+    {
+         BicicletaContext contextDB = new BicicletaContext();
+
+        public List<Bicicleta> GetBicicletas()
+        {
+            return contextDB.Bicicletas.ToList();
+        }
+        public void AdicionarBike(Bicicleta bike)
+        {
+            contextDB.Bicicletas.Add(bike);
+            contextDB.SaveChanges();
+        }
+
+        public void AlterarBicicleta(Bicicleta BicicletaEditada, int id)
+        {
+            Bicicleta bicicletaQueEuVouEditar = contextDB.Bicicletas.FirstOrDefault(p => p.Id == id);
+            bicicletaQueEuVouEditar = BicicletaEditada;
+            contextDB.SaveChanges();
+        }
+
+    }
+}
+
